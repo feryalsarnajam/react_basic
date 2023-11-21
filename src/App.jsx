6453e10981875859457ececc75c812d7ebf60d1b
Faryal Sarnajam <feryal_nl@yahoo.com>
2023-11-21 20:48:01 +0100

    recipe list  page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
-import { RecipeListPage } from './pages/RecipeListPage';
+import { RecipeListPage } from "./pages/RecipeListPage";
+import { RecipePage } from "./pages/RecipePage";
+import { useState } from "react";
+import { Center, Flex, Heading } from "@chakra-ui/react";
 
 export const App = () => {
-  // Your state code here
-  return <RecipeListPage />;
+  const [selectedRecipe, setSelectedRecipe] = useState();
+
+  const greeting = "Winc Recipe Checker";
+  return (
+    <>
+      <Flex  flexDir='column' bg='blue.500'>
+        {selectedRecipe ? (
+          <RecipePage item={selectedRecipe} clickFn={setSelectedRecipe} />
+        ) : (
+          <>
+            <Heading
+              size='2xl'
+              mb={8}
+              marginTop={8}
+              color='blue.50'
+              align='center'
+              justify='center'>
+              {greeting}
+            </Heading>
+            <RecipeListPage clickFn={setSelectedRecipe} />
+          </>
+        )}
+      </Flex>
+    </>
+  );
 };
